8bfa87ef19fb66bbb9469350e6ddd7460dd57425
metheman123 <99170574+metheman123@users.noreply.github.com>
2022-06-27 16:47:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+//namespacing
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground;
+var ball;
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  var ground_options = {
+    isStatic: true
+  }
+  ground = Bodies.rectangle(200, 390, 400, 10, ground_options);
+  World.add(world, ground);
+  var ball_options={
+    restitution:1
+  }
+  ball=Bodies.circle(250,200,10,ball_options);
+  World.add(world,ball);
+
+  
+}
+
+function draw() {
+  background(0);  
+  Engine.update(engine);
+  rectMode(CENTER);
+  rect(ground.position.x, ground.position.y, 400, 10);
+  ellipseMode(RADIUS);
+  ellipse(ball.position.x,ball.position.y,10,10);
+}
+
+/*
+1. Engine -> Universe (Matter.Engine)
+2. World -> Earth (Matter.World)
+3. Bodies -> US (Matter.Bodies)
+*/
